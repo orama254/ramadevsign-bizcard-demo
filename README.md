731b0b217dc4177abe271c4480d72afec345ac95
orama254 <orama209@gmail.com>
2021-03-23 14:44:30 +0300

    initial commit ramadevsign bizcard demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ramadevsign - Rama Ochieng
+
+This NPM package is a virtual business card for Rama Ochieng (ramadevsign).
---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`Rama Ochieng(ramadevsign)
+Website  :- https://www.ramadevsign.com
+LinkedIn :- https://linkedin.com/in/ramaochieng
+Twitter  :- https://www.twitter.com/ramaspeaksdev
+Github   :- https://www.github.com/orama254
+Behance  :- https://www.behance.net/ramaochieng
+Dribbble :- https://www.dribbble.com/ramadevsign
+Timezone :- EMEA (GMT to GMT+3)   
+Email    :- hello@ramadevsign.com`);
